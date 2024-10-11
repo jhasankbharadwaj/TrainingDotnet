ae53fc138ae930c878e25df5ee30932b93971bee
jhasankbharadwaj <218r5a6711@gmail.com>
2024-10-11 08:35:16 +0530

    two days commit

---
+++ day2_1010/WebApplication1/WebApplication1/Controllers/NOsciffController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{  //defining the uri in the requirement accoring to the output end point . 
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class NOsciffController : ControllerBase
+    {
+        //we can write the Get, post put ,patch methods can be writtern
+        [HttpGet]
+        public string Gethello()
+        {
+            return "hello world ";
+
+        }
+        [HttpGet]
+        public int GetMore()
+        {
+            return 90;
+        }
+    }
+}
