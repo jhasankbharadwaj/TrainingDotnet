3160990479d1f808b2ed08d137d78d2ede440019
jhasankbharadwaj <218r5a6711@gmail.com>
2024-10-13 10:50:41 +0530

    Paroduct added

---
+++ day4_12.10/ProductApi/Repository/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using ProductApi.Model;
+
+namespace ProductApi.Repository
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetProducts();
+
+        Product GetProductByID(int product);
+
+        void InsertProduct(Product product);
+
+        void DeleteProduct(int productId);
+
+        void UpdateProduct(Product product);
+
+        void Save();
+    }
+}
