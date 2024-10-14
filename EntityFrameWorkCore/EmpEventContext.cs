ac31da078cb7aa394fd01ce01f81a12b74d70931
jhasankbharadwaj <218r5a6711@gmail.com>
2024-10-14 07:17:48 +0530

    entityFramemvc

---
+++ EntityFrameWorkCore/EmpEventContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameWorkCore
+{
+    internal class EmpEventContext:DbContext
+    {
+      
+        public EmpEventContext(DbContextOptions<EmpEventContext> options):base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Event> Events { get; set; }
+
+    }
+}
---
+++ EntityFrameWorkCore/Program.cs
@@ -0,0 +1,18 @@
+﻿using EntityFrameWorkCore;
+
+internal class Program
+{
+    public  static void Main(string[] args)
+    {
+        using (EmpEventContext context = new EmpEventContext())
+        {
+            var emp=new Employee() { Id = 1 ,Name="jhasank",Experience=9};
+            context.Employees.Add(emp);
+            context.SaveChanges();
+            Console.Write("just added a employee ");
+        }
+
+      
+     }
+
+}
